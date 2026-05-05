af3a0d64a5fdfe45af19ad0a5194db3a58a10ece
instante <instanteinstante57@gmail.com>
2026-05-05 17:31:48 -0600

    fix: SoundCloud

--- server.js
+++ server.js
@@ -127,7 +127,7 @@ app.get("/playlist-progress", async (req, res) => {
             const proxyFlag = process.env.PROXY_URL ? `--proxy "${process.env.PROXY_URL}"` : '';
             const cookiesFlag = fs.existsSync(COOKIES_PATH) ? `--cookies ${COOKIES_PATH}` : '';
             const comando = esSpotify
-                ? `yt-dlp ${proxyFlag} ${cookiesFlag} -x --audio-format mp3 --no-playlist -o "${folderPath}/%(title)s.%(ext)s" "ytsearch1:${cancion}"`
+                ? `yt-dlp ${proxyFlag} ${cookiesFlag} -x --audio-format mp3 --no-playlist -o "${folderPath}/%(title)s.%(ext)s" "scsearch1:${cancion}"`
                 : `yt-dlp ${proxyFlag} ${cookiesFlag} -x --audio-format mp3 --no-playlist -o "${folderPath}/%(title)s.%(ext)s" "${cancion}"`;
 
             try {
